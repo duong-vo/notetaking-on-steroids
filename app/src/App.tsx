a1ff903fdf0bb4c7c69d7d62d41155890f603aab
duong-vo <vodh2@miamioh.edu>
2023-05-17 10:48:37 +0700

    migrate to react-d3-tree js

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,50 @@
-import { useState } from 'react'
-import Update from '@/components/update'
-import logoVite from './assets/logo-vite.svg'
-import logoElectron from './assets/logo-electron.svg'
-import './App.scss'
+import React from 'react';
+import Tree from 'react-d3-tree';
+import './tree.css';
+// This is a simplified example of an org chart with a depth of 2.
+// Note how deeper levels are defined recursively via the `children` property.
+const orgChart = {
+  name: 'CEO',
+  children: [
+    {
+      name: 'Manager',
+      attributes: {
+        department: 'Production',
+      },
+      children: [
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Fabrication',
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Assembly',
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function OrgChartTree() {
   return (
-    <div className='App'>
-      <button> Add a tree </button>
-      <button> Add a branch </button>
+    // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+    <div id="treeWrapper" style={{ width: '50em', height: '20em' }}>
+      <Tree data={orgChart} 
+      rootNodeClassName="node__root"/>
     </div>
-  )
+  );
 }
-
-export default App
